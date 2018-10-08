55f848166cc934b3ad856fa70e3f50e8201b6703
Jefinho97 <42986914+Jefinho97@users.noreply.github.com>
2018-10-08 16:41:41 -0400

    Initial commit

---
+++ Master/Master/View/MasterDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Master.View
+{
+	public class MasterDetail : MasterDetailPage
+	{
+		public MasterDetail ()
+		{
+            this.Master = new Menu();
+            this.Detail = new NavigationPage(new Pagina2());
+            App.MasterDetail = this;
+		}
+	}
+}
